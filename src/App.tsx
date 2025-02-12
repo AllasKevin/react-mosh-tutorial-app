8b88ed73c85817deb08ec8124d08dcde21d0cd76
AllasKevin <kevin_ruonala@hotmail.com>
2025-02-12 19:20:08 +0100

    setup of project and adding of Message component.

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Message from "./Message"
+// This is a component
+function App() { // we always have to close our react component, we could also use the self closing syntax <Message /> which is more consice.
+  return <div><Message></Message></div>
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+// this is now a very basic react component
+// PascalCasing
+function Message() {
+  // the syntax is JSX - JavaScript XML
+  const name = 'Kevin';
+  if (name)
+    return <h1> Hello {name}!</h1>
+  return <h1> Hello World!</h1>
+}
+
+export default Message;
