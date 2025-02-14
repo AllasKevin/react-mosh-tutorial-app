893f94541ca86ff7b27c72670135306141f6e624
AllasKevin <kevin_ruonala@hotmail.com>
2025-02-14 11:31:33 +0100

    before cleaning up code

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,7 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
-  const items = [
+  let items = [
     "New York",
     "San Francisco",
     "Tokyo",
@@ -7,13 +9,45 @@ function ListGroup() {
     "Stockholm",
     "London",
   ];
+  let selectedIndex = 0;
+  //items = [];
+  /*
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item found</p>
+      </>
+    );
+*/
+
+  const message = items.length === 0 ? <p>No item found</p> : null;
+
+  // The benefit of using a function to the constant above is that it can have parameters. But if you dont have a parameter then it is better to use a constant
+  const getMessage = () => {
+    items.length === 0 ? <p>No item found</p> : null;
+  };
+
+  // Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
+      {/*items.length === 0 ? <p>No item found</p> : null*/}
+      {/*message*/}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={handleClick}
+          >
             {item}
           </li>
         ))}
