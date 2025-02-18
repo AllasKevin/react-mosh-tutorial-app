33aefe7e48dae52ea5efca47b005e09b04a735b9
AllasKevin <kevin_ruonala@hotmail.com>
2025-02-18 18:25:08 +0100

    finished using CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Alert from "./components/Alert";
 import Button from "./components/Button";
 import DissmissableAlert from "./components/DissmissableAlert";
 import ListGroup from "./components/ListGroup";
+import "./App.css";
 
 // This is a component
 function App() {
