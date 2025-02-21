892983ef115140d31c357a875c72195be72cc5e5
AllasKevin <kevin_ruonala@hotmail.com>
2025-02-21 22:17:38 +0100

    finished applying validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,30 +1,45 @@
 import React, { FormEvent, useRef, useState } from "react";
 import { FieldValue, FieldValues, useForm } from "react-hook-form";
+
+interface FormData {
+  name: string;
+  age: number;
+}
 const Form = () => {
-  const { register, handleSubmit } = useForm();
-  //console.log(register("name"));
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  //console.log(errors);
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)} onClick={() => console.log(errors)}>
       <div className="mb-3">
         <label htmlFor="MyName" className="form-label">
           Name
         </label>
         <input
-          {...register("Name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="MyName"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">The name must be at least 3 characters</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="MyAge" className="form-label">
           Age
         </label>
         <input
-          {...register("Age")}
+          {...register("age")}
           id="MyAge"
           type="number"
           className="form-control"
