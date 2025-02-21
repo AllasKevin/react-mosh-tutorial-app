7a0909f7dcb1d53e823aab9e0e8f3ad57d1588a9
AllasKevin <kevin_ruonala@hotmail.com>
2025-02-21 10:32:48 +0100

    finished updating array of objects

--- src/App.tsx
+++ src/App.tsx
@@ -14,22 +14,20 @@ function App() {
     console.log(item);
   };
 
-  const [tags, setTags] = useState(["happy ", "cheerful "]);
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
 
   const HandleClick = () => {
-    // Add
-    //setTags([...tags, "exicited "]);
-
-    // Remove
-    //setTags(tags.filter((tag) => tag !== "happy "));
-
-    // Update
-    setTags(tags.map((tag) => (tag === "happy " ? "happiness " : tag)));
+    // Here a new array is created with a new bug object at the first index that has a modified value
+    // But the bug object at the second index will be the same object as in the original array, this will save memory.
+    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
   };
 
   return (
     <div>
-      {tags}
+      {bugs[0].fixed.toString()}
       <button onClick={HandleClick}>Click Me</button>
     </div>
   );
