7993da57bf9d58597320d7cce528e658d03cce82
AllasKevin <kevin_ruonala@hotmail.com>
2025-02-21 13:04:12 +0100

    finished exercise2 of updating state of array

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,19 @@ function App() {
     console.log(item);
   };
 
-  const [game, setGame] = useState({ id: 1, player: { name: "John" } });
-  const [savedGame] = useState(game);
+  const [pizza, setPizza] = useState({
+    name: "Kevins Special",
+    toppings: ["Mushroom"],
+  });
 
-  const changeName = () => {
-    // It is more future proof to add '...game.player' since if we ever change the player-object in the future we dont also have to change it here
-    setGame({ ...game, player: { ...game.player, name: "Kevin" } });
-    console.log("originalGame " + savedGame.player.name);
+  const addTopping = () => {
+    setPizza({ ...pizza, toppings: [...pizza.toppings, "Bacon"] });
   };
 
   return (
     <div>
-      {game.player.name}
-      <button onClick={changeName}>Change Name</button>
+      <p>{pizza.toppings.toString()}</p>
+      <button onClick={addTopping}>Change Name</button>
     </div>
   );
 }
