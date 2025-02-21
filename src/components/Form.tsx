89b7c6a71caa71a836303194dfea86b98f8d0f28
AllasKevin <kevin_ruonala@hotmail.com>
2025-02-21 18:13:56 +0100

    finished controlled components using useState instead of useRef to access input fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,15 +1,13 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  let person = { name: "", age: 0 };
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current !== null) person.name = nameRef.current.value;
-    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
-
     console.log(person);
   };
 
@@ -19,13 +17,29 @@ const Form = () => {
         <label htmlFor="MyName" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="MyName" type="text" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          id="MyName"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="MyAge" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="MyAge" type="number" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: event.target.value })
+          }
+          value={person.age}
+          id="MyAge"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
