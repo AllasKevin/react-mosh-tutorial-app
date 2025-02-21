59f05815ebc3fcbf2c837eef0b314e63d0177656
AllasKevin <kevin_ruonala@hotmail.com>
2025-02-21 22:41:13 +0100

    finished Schema based Validation with Zod

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,23 @@
 import React, { FormEvent, useRef, useState } from "react";
 import { FieldValue, FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface FormData {
-  name: string;
-  age: number;
-}
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters." }),
+  age: z
+    .number({ invalid_type_error: "Age field is required." })
+    .min(18, { message: "Your age must be at least 18." }),
+});
+// Detta är istället för att skapa ett FormData interface
+type FormData = z.infer<typeof schema>;
+
 const Form = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
   //console.log(errors);
 
   const onSubmit = (data: FieldValues) => console.log(data);
@@ -22,28 +29,24 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name", { required: true, minLength: 3 })}
+          {...register("name")}
           id="MyName"
           type="text"
           className="form-control"
         />
-        {errors.name?.type === "required" && (
-          <p className="text-danger">The name field is required</p>
-        )}
-        {errors.name?.type === "minLength" && (
-          <p className="text-danger">The name must be at least 3 characters</p>
-        )}
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="MyAge" className="form-label">
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { valueAsNumber: true })}
           id="MyAge"
           type="number"
           className="form-control"
         />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
