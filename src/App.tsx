8e9cf46e4e3fc0f82da600fdfd8463c31f61a6bd
AllasKevin <kevin_ruonala@hotmail.com>
2025-02-22 21:39:36 +0100

    finished understanding the effect hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,21 @@
-import { useCallback, useState } from "react";
-import produce from "immer";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
-import ExpandableText from "./components/ExpandableText";
-import Form from "./components/Form";
-import ExpenseForm from "./ExpenseTrackerProject/ExpenseForm";
-import ExpenseList from "./ExpenseTrackerProject/ExpenseList";
-import ExpenseFilter from "./ExpenseTrackerProject/ExpenseFilter";
-import categories from "./ExpenseTrackerProject/categories";
-
-interface item {
-  id: number;
-  description: string;
-  amount: number;
-  category: string;
-}
+import { useEffect, useRef } from "react";
 
 // This is a component
 function App() {
-  const [items, setItems] = useState<item[]>([]);
+  const ref = useRef<HTMLInputElement>(null);
 
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const visibleItems = selectedCategory
-    ? items.filter((item) => item.category === selectedCategory)
-    : items;
-
-  const deleteItem = (id: number) => {
-    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
-    console.log(items);
-  };
+  useEffect(() => {
+    // Side effect
+    if (ref.current) ref.current.focus();
+  });
+  useEffect(() => {
+    // Side effect
+    document.title = "My App";
+  });
 
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onAdd={(item) => setItems([...items, { ...item, id: Math.random() }])}
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(selected) => setSelectedCategory(selected)}
-        />
-      </div>
-      <ExpenseList onDelete={(id) => deleteItem(id)} items={visibleItems} />
+      <input ref={ref} type="text" className="form-control" />
     </div>
   );
 }
